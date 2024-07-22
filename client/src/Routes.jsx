ce9a3de439c0494a3d50b21c7f3f35e1ff071ac9
shubham Rawat <rawatshubam269@gmail.com>
2024-07-22 11:32:07 +0530

    Basic Setup + Route & Page Setup + Neccessary Dep. Included

---
+++ client/src/Routes.jsx
@@ -0,0 +1,11 @@
+import { Home, About, Dashboard, Projects, Signin, Signup } from "./pages";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    children: [],
+  },
+];
+
+export default routes;
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./home/Home";
+import About from "./about/About";
+import Dashboard from "./dashboard/Dashboard";
+import Projects from "./projects/Projects";
+import Signin from "./signin/Signin";
+import Signup from "./signup/Signup";
+
+export { Home, About, Dashboard, Projects, Signin, Signup };
