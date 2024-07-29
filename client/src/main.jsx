049f009899b586d69d4936e599bba177f6a8102a
shubham Rawat <rawatshubam269@gmail.com>
2024-07-29 12:03:48 +0530

    Redux persist done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,18 +5,20 @@ import routes from "./Routes";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
-
+import { PersistGate } from "redux-persist/integration/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-      <Toaster position="top-right" reverseOrder={false} />
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+        <Toaster position="top-right" reverseOrder={false} />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
